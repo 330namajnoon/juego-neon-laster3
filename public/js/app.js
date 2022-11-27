291e5b4a677d87673b6f77265d7e4eea64fd6b56
30namajnoon <sina.majnoonhjk@gmail.com>
2022-11-27 17:56:53 +0100

    1

--- public/js/app.js
+++ public/js/app.js
@@ -4,19 +4,45 @@ myimage.src = "../images/neon.png";
 
 function Myjet() {
     this.dx = 39;
-    this.dy = 363;
-    this.jx = 100;
-    this.jy = 100;
+    this.dy = 360;
+    this.jx = innerWidth/2-35;
+    this.jy = innerHeight-130;
     this.w = 74;
     this.h = 74;
+    this.s = 10;
+
+    window.addEventListener("keydown", (e) => {
+
+        switch (e.keyCode) {
+            case 37:
+                if (this.jx > 0) this.jx -= this.s;
+                break;
+            case 38:
+                if(this.jy > 0) this.jy -= this.s;
+                break;
+            case 39:
+                if(this.jx + this.w < innerWidth)this.jx += this.s;
+                break;
+            case 40:
+                if(this.jy + this.h*2 < innerHeight)this.jy += this.s;
+                break;
+            case 32:
+
+                break;
+
+        }
+
+    })
+
 
 }
-Myjet.prototype.draw = function() {
-    canvas.ctx.drawImage(myimage,this.dx,this.dy,this.w,this.h,this.jx,this.jy,this.w,this.h);
-
+Myjet.prototype.draw = function () {
+    canvas.ctx.drawImage(myimage, this.dx, this.dy, this.w, this.h, this.jx, this.jy, this.w, this.h);
+    canvas.ctx.drawImage(myimage, 70, 670, 32, 52, this.jx + 18, this.jy + this.h - 5, 32, 52);
 }
-Myjet.prototype.update = function() {
-    
+Myjet.prototype.update = function () {
+
+
 }
 
 
@@ -25,15 +51,15 @@ function Canvas() {
     this.canvas = document.querySelector("canvas");
     this.ctx = this.canvas.getContext("2d");
 
-    this.canvas.width = innerWidth-10;
-    this.canvas.height = innerHeight-10;
+    this.canvas.width = innerWidth - 10;
+    this.canvas.height = innerHeight - 10;
 
-    this.myjet  = new Myjet();
+    this.myjet = new Myjet();
 }
-Canvas.prototype.draw = function() {
+Canvas.prototype.draw = function () {
     this.myjet.draw();
 }
-Canvas.prototype.update = function() {
+Canvas.prototype.update = function () {
     this.myjet.update();
 }
 let canvas = new Canvas();
@@ -44,7 +70,7 @@ let canvas = new Canvas();
 
 
 function anim() {
-    canvas.ctx.clearRect(0,0,innerWidth,innerHeight);
+    canvas.ctx.clearRect(0, 0, innerWidth, innerHeight);
     canvas.draw();
     canvas.update();
     requestAnimationFrame(anim);
